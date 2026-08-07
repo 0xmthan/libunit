a2d583522b73c4dde1489f1cb9b6efb119fce58b
mthan <me@mthan.dev>
2026-08-07 22:47:47 +0300

    test/use fork()

--- main.c
+++ main.c
@@ -6,13 +6,25 @@
 /*   By: mtaheri@student.42istanbul.com.tr          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/07 21:59:06 by mtaheri           #+#    #+#             */
-/*   Updated: 2026/08/07 22:00:03 by mtaheri          ###   ########.fr       */
+/*   Updated: 2026/08/07 22:47:15 by mtaheri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
-int main ()
+#include <stdio.h>
+
+int	main(void)
 {
-	ft_putstr_fd("test string\n", 1);
+	pid_t	p;
+
+	p = fork();
+	if (p < 0)
+	{
+		perror("fork fail");
+		exit(1);
+	}
+	ft_putstr_fd("test fork\n", 1);
+	ft_putnbr_fd(getpid(), 1);
+	ft_putchar_fd('\n', 1);
 	return (0);
 }
