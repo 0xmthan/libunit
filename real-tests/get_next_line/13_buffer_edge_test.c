a6e116afa3bf078eea1e3e73b108d85f2c2bc6ff
Kadir Eren Altintas <kadirerenaltintas@hotmail.com.tr>
2026-08-09 21:54:26 +0300

    improve gnl edge case tests

--- real-tests/get_next_line/13_buffer_edge_test.c
+++ real-tests/get_next_line/13_buffer_edge_test.c
@@ -5,32 +5,26 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: kaaltint@student.42istanbul.com.tr         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/08/09 21:36:42 by kaaltint          #+#    #+#             */
-/*   Updated: 2026/08/09 21:36:44 by kaaltint         ###   ########.fr       */
+/*   Created: 2026/08/09 21:52:13 by kaaltint          #+#    #+#             */
+/*   Updated: 2026/08/09 21:52:14 by kaaltint         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "gnl_test.h"
 
-int	mixed_lengths_test(void)
+int	buffer_edge_test(void)
 {
 	int		fd;
-	char	*l1;
-	char	*l2;
-	char	*l3;
+	char	*line;
 	char	*expected;
 
-	expected = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-		"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789\n";
-	fd = open("get_next_line/files/mixed_lengths.txt", O_RDONLY);
+	expected = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n";
+	fd = open("get_next_line/files/buffer_edge.txt", O_RDONLY);
 	if (fd < 0)
 		return (-1);
-	l1 = get_next_line(fd);
-	l2 = get_next_line(fd);
-	l3 = get_next_line(fd);
-	if (l1 && l2 && l3 && ft_strcmp(l1, "a\n") == 0
-		&& ft_strcmp(l2, expected) == 0 && ft_strcmp(l3, "end") == 0)
-		return (free(l1), free(l2), free(l3), close(fd), 0);
+	line = get_next_line(fd);
+	if (line && ft_strcmp(line, expected) == 0)
+		return (free(line), close(fd), 0);
 	else
-		return (free(l1), free(l2), free(l3), close(fd), -1);
+		return (free(line), close(fd), -1);
 }
--- real-tests/get_next_line/14_mixed_lengths_test.c
+++ real-tests/get_next_line/14_mixed_lengths_test.c
@@ -1,32 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   14_mixed_lines_test.c                              :+:      :+:    :+:   */
+/*   14_mixed_lengths_test.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mtaheri@student.42istanbul.com.tr          +#+  +:+       +#+        */
+/*   By: kaaltint@student.42istanbul.com.tr         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/08/08 21:51:04 by kaaltint          #+#    #+#             */
-/*   Updated: 2026/08/09 17:31:48 by mtaheri          ###   ########.fr       */
+/*   Created: 2026/08/09 21:53:49 by kaaltint          #+#    #+#             */
+/*   Updated: 2026/08/09 21:53:51 by kaaltint         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "gnl_test.h"
 
-int	mixed_lines_test(void)
+int	mixed_lengths_test(void)
 {
 	int		fd;
 	char	*l1;
 	char	*l2;
 	char	*l3;
+	char	*expected;
 
-	fd = open("get_next_line/files/mixed_lines.txt", O_RDONLY);
+	expected = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+		"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789\n";
+	fd = open("get_next_line/files/mixed_lengths.txt", O_RDONLY);
 	if (fd < 0)
 		return (-1);
 	l1 = get_next_line(fd);
 	l2 = get_next_line(fd);
 	l3 = get_next_line(fd);
-	if (l1 && l2 && l3 && ft_strcmp(l1, "\n") == 0
-		&& ft_strcmp(l2, "hello\n") == 0 && ft_strcmp(l3, "\n") == 0)
+	if (l1 && l2 && l3 && ft_strcmp(l1, "a\n") == 0
+		&& ft_strcmp(l2, expected) == 0 && ft_strcmp(l3, "end") == 0)
 		return (free(l1), free(l2), free(l3), close(fd), 0);
 	else
 		return (free(l1), free(l2), free(l3), close(fd), -1);
